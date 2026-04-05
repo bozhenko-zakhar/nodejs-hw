9b31574235d3d8398140149de93c520c1a2521f9
Bozhenko Zakhar <zakhar1bozhenko@gmail.com>
2026-04-05 22:31:45 +0200

    Upload homework

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,9 +1,12 @@
 import { Router } from 'express';
-import { getNotes, getNotesById } from '../controllers/notesController';
+import { getAllNotes, getNotesById, createNote, deleteNote, updateNote } from '../controllers/notesController.js';
 
 const router = Router();
 
-router.get("/notes", getNotes);
+router.get("/notes", getAllNotes);
 router.get("/notes/:noteId", getNotesById);
+router.post("/notes", createNote);
+router.delete("/notes/:noteId", deleteNote);
+router.patch("/notes/:noteId", updateNote);
 
 export default router;
--- src/server.js
+++ src/server.js
@@ -1,15 +1,16 @@
 import express from "express";
 import cors from 'cors';
 import 'dotenv/config';
-import { connectMongoDB } from "./db/connectMongoDB";
-import { logger } from "./middleware/logger";
-import { errorHandler } from "./middleware/errorHandler";
-import { notFoundHandler } from "./middleware/notFoundHandler";
-import notesRoutes from "./routes/notesRoutes";
+import { connectMongoDB } from "./db/connectMongoDB.js";
+import { logger } from "./middleware/logger.js";
+import { errorHandler } from "./middleware/errorHandler.js";
+import { notFoundHandler } from "./middleware/notFoundHandler.js";
+import notesRoutes from "./routes/notesRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
+// Setting's middlewares
 app.use(logger);
 app.use(express.json());
 app.use(cors());
@@ -19,12 +20,10 @@ app.use(notesRoutes);
 
 // 404 & Error Middlewares
 app.use(notFoundHandler);
-
 app.use(errorHandler);
 
+// Server connections
 await connectMongoDB();
-
-// Server launching
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
